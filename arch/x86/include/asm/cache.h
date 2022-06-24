0277177ce93c0c11d4be04ad765d975ab5ff247b
Dongli Si <sdl@radxa.com>
2022-06-24 18:41:49 +0800

    arch/x86/Kconfig.cpu

--- arch/x86/include/asm/cache.h
+++ arch/x86/include/asm/cache.h
@@ -5,12 +5,12 @@
 #include <linux/linkage.h>
 
 /* L1 cache line size */
-#define L1_CACHE_SHIFT	(CONFIG_X86_L1_CACHE_SHIFT)
+#define L1_CACHE_SHIFT	6
 #define L1_CACHE_BYTES	(1 << L1_CACHE_SHIFT)
 
 #define __read_mostly __section(".data..read_mostly")
 
-#define INTERNODE_CACHE_SHIFT CONFIG_X86_INTERNODE_CACHE_SHIFT
+#define INTERNODE_CACHE_SHIFT 6
 #define INTERNODE_CACHE_BYTES (1 << INTERNODE_CACHE_SHIFT)
 
 #ifdef CONFIG_X86_VSMP
