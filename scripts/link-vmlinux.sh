2c290aa45a96a70f9d8bba02cd86b8d5c10070f7
Dongli Si <sdl@radxa.com>
2022-06-29 22:58:56 +0800

    u

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -12,7 +12,6 @@ info()
 	printf "  %-7s %s\n" "${1}" "${2}"
 }
 
-
 # Create map file with all symbols from ${1}
 # See mksymap for additional details
 mksysmap()
@@ -25,13 +24,6 @@ ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init need-builtin=1
 
 #link vmlinux.o
 ${MAKE} -f "${srctree}/scripts/Makefile.vmlinux_o"
-
-info MODINFO modules.builtin.modinfo
-${OBJCOPY} -j .modinfo -O binary vmlinux.o modules.builtin.modinfo
-info GEN modules.builtin
-# The second line aids cases where multiple modules share the same object.
-tr '\0' '\n' < modules.builtin.modinfo | sed -n 's/^[[:alnum:]:_]*\.file=//p' |
-	tr ' ' '\n' | uniq | sed -e 's:^:kernel/:' -e 's/$/.ko/' > modules.builtin
 
 ${LD} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux} --script=${objtree}/${KBUILD_LDS} --strip-debug -o vmlinux --whole-archive ${KBUILD_VMLINUX_OBJS} --no-whole-archive --start-group ${KBUILD_VMLINUX_LIBS} --end-group
 
