a5b6e6229bedba0e7812480cca44988f027a35a7
Dongli Si <sdl@radxa.com>
2022-06-29 22:57:38 +0800

    u

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -12,60 +12,6 @@ info()
 	printf "  %-7s %s\n" "${1}" "${2}"
 }
 
-# Link of vmlinux
-# ${1} - output file
-# ${2}, ${3}, ... - optional extra .o files
-vmlinux_link()
-{
-	local output=${1}
-	local objs
-	local libs
-	local ld
-	local ldflags
-	local ldlibs
-
-	info LD ${output}
-
-	# skip output file argument
-	shift
-
-		objs="${KBUILD_VMLINUX_OBJS}"
-		libs="${KBUILD_VMLINUX_LIBS}"
-
-	if [ "${SRCARCH}" = "um" ]; then
-		wl=-Wl,
-		ld="${CC}"
-		ldflags="${CFLAGS_vmlinux}"
-		ldlibs="-lutil -lrt -lpthread"
-	else
-		wl=
-		ld="${LD}"
-		ldflags="${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux}"
-		ldlibs=
-	fi
-
-	ldflags="${ldflags} ${wl}--script=${objtree}/${KBUILD_LDS}"
-
-	# The kallsyms linking does not need debug symbols included.
-	if [ "$output" != "${output#.tmp_vmlinux.kallsyms}" ] ; then
-		ldflags="${ldflags} ${wl}--strip-debug"
-	fi
-
-	${ld} ${ldflags} -o ${output}					\
-		${wl}--whole-archive ${objs} ${wl}--no-whole-archive	\
-		${wl}--start-group ${libs} ${wl}--end-group		\
-		$@ ${ldlibs}
-}
-
-# Create ${2} .S file with all symbols from the ${1} object file
-kallsyms()
-{
-	local kallsymopt;
-
-	info KSYMS ${2}
-	${NM} -n ${1} | scripts/kallsyms ${kallsymopt} > ${2}
-}
-
 
 # Create map file with all symbols from ${1}
 # See mksymap for additional details
@@ -80,7 +26,6 @@ ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init need-builtin=1
 #link vmlinux.o
 ${MAKE} -f "${srctree}/scripts/Makefile.vmlinux_o"
 
-
 info MODINFO modules.builtin.modinfo
 ${OBJCOPY} -j .modinfo -O binary vmlinux.o modules.builtin.modinfo
 info GEN modules.builtin
@@ -88,21 +33,10 @@ info GEN modules.builtin
 tr '\0' '\n' < modules.builtin.modinfo | sed -n 's/^[[:alnum:]:_]*\.file=//p' |
 	tr ' ' '\n' | uniq | sed -e 's:^:kernel/:' -e 's/$/.ko/' > modules.builtin
 
-
-btf_vmlinux_bin_o=""
-
-kallsymso=""
-kallsymso_prev=""
-kallsyms_vmlinux=""
-
-vmlinux_link vmlinux "${kallsymso}" ${btf_vmlinux_bin_o}
-
-# fill in BTF IDs
+${LD} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux} --script=${objtree}/${KBUILD_LDS} --strip-debug -o vmlinux --whole-archive ${KBUILD_VMLINUX_OBJS} --no-whole-archive --start-group ${KBUILD_VMLINUX_LIBS} --end-group
 
 info SYSMAP System.map
 mksysmap vmlinux System.map
-
-# step a (see comment above)
 
 # For fixdep
 echo "vmlinux: $0" > .vmlinux.d
