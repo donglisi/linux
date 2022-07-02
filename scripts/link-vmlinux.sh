b73e28438558561dbee6c25e0083237d65d4a142
Dongli Si <sidongli1997@gmail.com>
2022-07-02 21:01:47 +0800

    u

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -7,7 +7,7 @@ LDFLAGS_vmlinux="$3"
 
 ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init
 
-${MAKE} -f "${srctree}/scripts/Makefile.vmlinux_o"
+# ${MAKE} -f "${srctree}/scripts/Makefile.vmlinux_o"
 
 ${LD} ${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux} --script=${objtree}/${KBUILD_LDS} --strip-debug -o vmlinux --whole-archive ${KBUILD_VMLINUX_OBJS} --no-whole-archive --start-group ${KBUILD_VMLINUX_LIBS} --end-group
 
