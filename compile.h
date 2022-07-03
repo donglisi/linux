f8cae4219836576b15a913feffeb2d85784992f7
Dongli Si <sidongli1997@gmail.com>
2022-07-03 18:15:22 +0800

    u

---
+++ compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#5 SMP Sat Jul 2 11:35:53 CST 2022"
+#define LINUX_COMPILE_BY "d"
+#define LINUX_COMPILE_HOST "develop"
+#define LINUX_COMPILER "gcc (GCC) 12.0.1 20220413 (Red Hat 12.0.1-0), GNU ld version 2.37-24.fc36"
