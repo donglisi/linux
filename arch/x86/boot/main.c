15086123c3e8d64ada10690fa1788b90dd4664d3
Dongli Si <sidongli1997@gmail.com>
2022-08-27 16:11:31 +0800

    rm arch/x86/boot/memory.c

--- arch/x86/boot/main.c
+++ arch/x86/boot/main.c
@@ -1,42 +1,39 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/* -*- linux-c -*- ------------------------------------------------------- *
- *
- *   Copyright (C) 1991, 1992 Linus Torvalds
- *   Copyright 2007 rPath, Inc. - All Rights Reserved
- *   Copyright 2009 Intel Corporation; author H. Peter Anvin
- *
- * ----------------------------------------------------------------------- */
-
-/*
- * Main module for the real-mode kernel code
- */
-#include <linux/build_bug.h>
-
 #include "boot.h"
 #include <string.h>
 
+#define SMAP	0x534d4150	/* ASCII "SMAP" */
+
 struct boot_params boot_params __attribute__((aligned(16)));
 
-static void copy_boot_params(void)
+static void detect_memory_e820(void)
 {
-	struct old_cmdline {
-		u16 cl_magic;
-		u16 cl_offset;
-	};
-	const struct old_cmdline * const oldcmd =
-		absolute_pointer(OLD_CL_ADDRESS);
+	int count = 0;
+	struct biosregs ireg, oreg;
+	struct boot_e820_entry *desc = boot_params.e820_table;
+	static struct boot_e820_entry buf; /* static so it is zeroed */
 
-	BUILD_BUG_ON(sizeof(boot_params) != 4096);
-	memcpy(&boot_params.hdr, &hdr, sizeof(hdr));
+	initregs(&ireg);
+	ireg.ax  = 0xe820;
+	ireg.cx  = sizeof(buf);
+	ireg.edx = SMAP;
+	ireg.di  = (size_t)&buf;
+
+	do {
+		intcall(0x15, &ireg, &oreg);
+		ireg.ebx = oreg.ebx; /* for next iteration... */
+
+		*desc++ = buf;
+		count++;
+	} while (ireg.ebx && count < ARRAY_SIZE(boot_params.e820_table));
+
+	boot_params.e820_entries = count;
 }
 
 void main(void)
 {
-	/* First, copy the boot header into the "zeropage" */
-	copy_boot_params();
+	memcpy(&boot_params.hdr, &hdr, sizeof(hdr));
 
-	/* Detect memory layout */
-	detect_memory();
+	detect_memory_e820();
 
 	go_to_protected_mode();
 }
