fce5e60dca1d5d3df7c270d1bed650dab37c2571
Dongli Si <sidongli1997@gmail.com>
2022-09-04 03:13:48 +0800

    fix run

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,6 @@
-/* This file is auto generated, version 5 */
-/* SMP */
+/* This file is auto generated, version 48 */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#5 SMP Sat Jul 2 11:35:53 CST 2022"
+#define UTS_VERSION "#48 Sat Sep 3 23:34:37 CST 2022"
 #define LINUX_COMPILE_BY "d"
 #define LINUX_COMPILE_HOST "develop"
 #define LINUX_COMPILER "gcc (GCC) 12.0.1 20220413 (Red Hat 12.0.1-0), GNU ld version 2.37-24.fc36"
