f954df4ca57f53712f38784750806bad1f544d1d
Dongli Si <sidongli1997@gmail.com>
2022-09-10 05:24:51 +0800

    arch/x86/mm/init.c

--- arch/x86/mm/init.c
+++ arch/x86/mm/init.c
@@ -434,25 +434,7 @@ void __init init_mem_mapping(void)
 
 	end = max_pfn << PAGE_SHIFT;
 
-	/*
-	 * If the allocation is in bottom-up direction, we setup direct mapping
-	 * in bottom-up, otherwise we setup direct mapping in top-down.
-	 */
-	if (memblock_bottom_up()) {
-		unsigned long kernel_end = __pa_symbol(_end);
-
-		/*
-		 * we need two separate calls here. This is because we want to
-		 * allocate page tables above the kernel. So we first map
-		 * [kernel_end, end) to make memory above the kernel be mapped
-		 * as soon as possible. And then use page tables allocated above
-		 * the kernel to map [ISA_END_ADDRESS, kernel_end).
-		 */
-		memory_map_bottom_up(kernel_end, end);
-		memory_map_bottom_up(ISA_END_ADDRESS, kernel_end);
-	} else {
-		memory_map_top_down(ISA_END_ADDRESS, end);
-	}
+	memory_map_top_down(0, end);
 
 	if (max_pfn > max_low_pfn) {
 		/* can we preserve max_low_pfn ?*/
