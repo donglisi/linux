48758b412f6d698bd0d1a5b33fc796a4b723df4f
Dongli Si <sidongli1997@gmail.com>
2022-09-09 12:14:26 +0800

    arch/x86/kernel/e820.c

--- arch/x86/kernel/e820.c
+++ arch/x86/kernel/e820.c
@@ -1,14 +1,3 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Low level x86 E820 memory map handling functions.
- *
- * The firmware and bootloader passes us the "E820 table", which is the primary
- * physical memory layout description available about x86 systems.
- *
- * The kernel takes the E820 memory layout and optionally modifies it with
- * quirks and other tweaks, and feeds that into the generic Linux memory
- * allocation code routines via a platform independent interface (memblock, etc.).
- */
 #include <linux/crash_dump.h>
 #include <linux/memblock.h>
 #include <linux/suspend.h>
@@ -20,88 +9,9 @@
 #include <asm/e820/api.h>
 #include <asm/setup.h>
 
-/*
- * We organize the E820 table into three main data structures:
- *
- * - 'e820_table_firmware': the original firmware version passed to us by the
- *   bootloader - not modified by the kernel. It is composed of two parts:
- *   the first 128 E820 memory entries in boot_params.e820_table and the remaining
- *   (if any) entries of the SETUP_E820_EXT nodes. We use this to:
- *
- *       - inform the user about the firmware's notion of memory layout
- *         via /sys/firmware/memmap
- *
- *       - the hibernation code uses it to generate a kernel-independent CRC32
- *         checksum of the physical memory layout of a system.
- *
- * - 'e820_table_kexec': a slightly modified (by the kernel) firmware version
- *   passed to us by the bootloader - the major difference between
- *   e820_table_firmware[] and this one is that, the latter marks the setup_data
- *   list created by the EFI boot stub as reserved, so that kexec can reuse the
- *   setup_data information in the second kernel. Besides, e820_table_kexec[]
- *   might also be modified by the kexec itself to fake a mptable.
- *   We use this to:
- *
- *       - kexec, which is a bootloader in disguise, uses the original E820
- *         layout to pass to the kexec-ed kernel. This way the original kernel
- *         can have a restricted E820 map while the kexec()-ed kexec-kernel
- *         can have access to full memory - etc.
- *
- * - 'e820_table': this is the main E820 table that is massaged by the
- *   low level x86 platform code, or modified by boot parameters, before
- *   passed on to higher level MM layers.
- *
- * Once the E820 map has been converted to the standard Linux memory layout
- * information its role stops - modifying it has no effect and does not get
- * re-propagated. So itsmain role is a temporary bootstrap storage of firmware
- * specific memory layout data during early bootup.
- */
 static struct e820_table e820_table_init		__initdata;
-static struct e820_table e820_table_kexec_init		__initdata;
-static struct e820_table e820_table_firmware_init	__initdata;
 
 struct e820_table *e820_table __refdata			= &e820_table_init;
-struct e820_table *e820_table_kexec __refdata		= &e820_table_kexec_init;
-struct e820_table *e820_table_firmware __refdata	= &e820_table_firmware_init;
-
-/* For PCI or other memory-mapped resources */
-unsigned long pci_mem_start = 0xaeedbabe;
-#ifdef CONFIG_PCI
-EXPORT_SYMBOL(pci_mem_start);
-#endif
-
-/*
- * This function checks if any part of the range <start,end> is mapped
- * with type.
- */
-static bool _e820__mapped_any(struct e820_table *table,
-			      u64 start, u64 end, enum e820_type type)
-{
-	int i;
-
-	for (i = 0; i < table->nr_entries; i++) {
-		struct e820_entry *entry = &table->entries[i];
-
-		if (type && entry->type != type)
-			continue;
-		if (entry->addr >= end || entry->addr + entry->size <= start)
-			continue;
-		return true;
-	}
-	return false;
-}
-
-bool e820__mapped_raw_any(u64 start, u64 end, enum e820_type type)
-{
-	return _e820__mapped_any(e820_table_firmware, start, end, type);
-}
-EXPORT_SYMBOL_GPL(e820__mapped_raw_any);
-
-bool e820__mapped_any(u64 start, u64 end, enum e820_type type)
-{
-	return _e820__mapped_any(e820_table, start, end, type);
-}
-EXPORT_SYMBOL_GPL(e820__mapped_any);
 
 /*
  * Add a memory region to the kernel E820 map.
@@ -362,9 +272,6 @@ void __init e820__memory_setup(void)
 	BUILD_BUG_ON(sizeof(struct boot_e820_entry) != 20);
 
 	who = x86_init.resources.memory_setup();
-
-	memcpy(e820_table_kexec, e820_table, sizeof(*e820_table_kexec));
-	memcpy(e820_table_firmware, e820_table, sizeof(*e820_table_firmware));
 
 	pr_info("BIOS-provided physical RAM map:\n");
 	e820__print_table(who);
